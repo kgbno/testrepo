5511e865acb6139c4242b1b8541b04030ba18039
MAJID <majidhans25@gmail.com>
2020-01-11 14:54:48 +0500

    title add

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,6 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<title>Welcome to Development</title>
 	<h1>Welcome to BEST HISAB.COM
 	</h1>
+	<title> majid ali </title>
 
 	<style type="text/css">
 
